848c78e5f401364eed048d597d874bc25a0e1035
Amanullah <amansoomro062@gmail.com>
2020-11-08 15:36:35 +0500

    Added fields to checkout form

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {CartService} from '../../services/cart.service';
 
 @Component({
   selector: 'app-checkout',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./checkout.component.css']
 })
 export class CheckoutComponent implements OnInit {
+  checkoutFormGroup: FormGroup;
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
 
-  constructor() { }
+
+
+  constructor(private formBuilder: FormBuilder,
+              private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.checkoutFormGroup = this.formBuilder.group({
+      customer: this.formBuilder.group({
+        firstName: [''],
+        lastName: [''],
+        email: ['']
+      }),
+      shippingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      billingAddress: this.formBuilder.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        country: [''],
+        zipCode: [''],
+      }),
+      creditCardInformation: this.formBuilder.group({
+        cardType: [''],
+        nameOnCard: [''],
+        cardNumber: [''],
+        securityCode: [''],
+        expirationMonth: [''],
+        expirationYear: [''],
+      }),
+    });
   }
 
+  onSubmit() {
+    console.log("They this is a form submission!");
+  }
+
+  copyShippingAddressToBillingAddress() {
+  }
 }
